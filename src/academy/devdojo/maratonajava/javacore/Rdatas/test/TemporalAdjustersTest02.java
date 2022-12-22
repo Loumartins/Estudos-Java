4b9802300b63a001fd6c477fa5170683d402d385
Luiz <luizhmartins11@gmail.com>
2022-12-22 10:44:58 -0300

    Exercicio relacionado a Temporal Adjusters, onde ira mostrar o proximo dia útil - Java

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/TemporalAdjustersTest02.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import academy.devdojo.maratonajava.javacore.Gassosiacao.dominio.Local;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.*;
+
+class ObterProximoDiaUtil implements TemporalAdjuster{
+
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+        DayOfWeek dayOfWeek = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+        int addDays;
+        switch (dayOfWeek){
+            case THURSDAY: addDays = 4; break;
+            case FRIDAY : addDays = 3;break;
+            case SATURDAY: addDays = 2;break;
+            default: addDays=1;
+        }
+        return temporal.plus(addDays, ChronoUnit.DAYS);
+    }
+}
+
+public class TemporalAdjustersTest02 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(new ObterProximoDiaUtil());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().withDayOfMonth(10).with(new ObterProximoDiaUtil());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+}
